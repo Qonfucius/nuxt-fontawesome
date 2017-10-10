4af39b28dd093ee8d5f3217d90766d6a0da16a04
Gaël <gael@hungryup.co>
2017-10-11 00:16:27 +0200

    🎉 First commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  // required to lint *.vue files
+  plugins: [
+    'html',
+  ],
+  // add your custom rules here
+  rules: {
+    'import/no-dynamic-require': 0,
+    'global-require': 0,
+
+  },
+  globals: {},
+};
