ae8c564f47733d6e7151728c8e73e8d25efe4540
Gaël <gael@hungryup.co>
2017-10-11 01:01:34 +0200

    ✨ Let using config to add packs

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-module.exports = async function nuxtFontAwesome() {
-  // something
+const { join } = require('path');
+
+module.exports = async function nuxtFontAwesome(moduleOptions) {
+  const defaultOptions = {
+    componentName: 'fa-icon',
+    packs: [],
+  };
+  const options = Object.assign(defaultOptions, this.options.fontAwesome, moduleOptions);
+
+  this.addPlugin({
+    src: join(__dirname, 'plugin.js'),
+    options,
+  });
 };
---
+++ plugin.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'; // eslint-disable-line
+import fontawesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
+<% options.packs.forEach(({package, icons}) => { %>
+  <% if (icons) { %>
+    import  { <%=icons.join(',')%> } from '<%=package%>';
+  <% } else { %>
+    import <%=package.split(/[\s\/]+/).replace(/-/g, "")%> from '<%=package%>';
+  <% } %>
+<% }) %>
+
+<% options.packs.forEach(({package, icons}) => { %>
+<% if (icons) { %>
+    fontawesome.library.add(<%=icons.join(',')%>);
+  <% } else { %>
+    fontawesome.library.add(<%=package.split(/[\s\/]+/).replace(/-/g, "")%>);
+  <% } %>
+<% }) %>
+
+Vue.component('<%=options.componentName%>', FontAwesomeIcon);
