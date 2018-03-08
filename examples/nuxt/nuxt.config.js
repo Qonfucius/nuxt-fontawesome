fe13317701a343214c5731ff715fd9ae0928bd48
Gaël <gael@hungryup.co>
2018-03-08 22:08:34 +0100

    Simple example

---
+++ examples/nuxt/nuxt.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  modules: ['qonfucius-nuxt-fontawesome'],
+  fontAwesome: {
+    packs: [
+      {
+        package: '@fortawesome/fontawesome-free-brands',
+        icons: ['faVuejs'],
+      },
+    ],
+  },
+}
---
+++ examples/nuxt/pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <h1>A simple working example</h1>
+    <fa-icon pack="fab" name="vuejs" />
+  </main>
+</template>
+
+<style>
+  .fa-vuejs {
+    width: 250px !important;
+    height: 250px !important;
+    color: aquamarine;
+  }
+</style>
